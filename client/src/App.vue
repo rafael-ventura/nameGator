fe595d50ec9d444cd5885529bb27e96baf6241ae
Rafael Ventura <raf4ael@gmail.com>
2020-12-20 23:25:01 -0300

    adicionando arquivos frontend

---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div id="slogan">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js</h6>
+    </div>
+    <div id="main">
+      <AppDomainList></AppDomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import AppDomainList from './components/AppDomainList.vue';
+
+export default {
+        name: 'App',
+        components: {
+                AppDomainList,
+        },
+};
+</script>
+
+<style>
+#slogan {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #17a2b8;
+  margin-top: 60px;
+}
+#main {
+  background-color: lightgray;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
---
+++ client/src/components/AppDomainList.vue
@@ -0,0 +1,177 @@
+<template>
+  <div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <AppItemList
+              title="Prefixo"
+              v-bind:items="prefixes"
+              v-on:addItem="addPrefix"
+              v-on:deleteItem="deletePrefix"
+            ></AppItemList>
+          </div>
+          <div class="col-md">
+            <AppItemList
+              title="Sufixo"
+              v-bind:items="sufixes"
+              v-on:addItem="addSufix"
+              v-on:deleteItem="deleteSufix"
+            ></AppItemList>
+          </div>
+        </div>
+        <br />
+        <h5>
+          Dominios <span class="badge badge-info"> {{ domains.length }}</span>
+        </h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li
+                class="list-group-item"
+                v-for="domain in domains"
+                v-bind:key="domain.name"
+              >
+                <div class="row">
+                  <div class="col-md">
+                    {{ domain.name }}
+                  </div>
+                  <div class="col-md text-right">
+                    <a
+                      class="btn btn-info"
+                      v-bind:href="domain.checkout"
+                      target="_blank"
+                    >
+                      <span class="fa fa-shopping-cart"> </span>
+                    </a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios/dist/axios';
+import AppItemList from './AppItemList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppItemList,
+  },
+  data: function() {
+    return {
+      prefixes: [],
+      sufixes: [],
+    };
+  },
+  methods: {
+    addPrefix(prefix) {
+      axios({
+        url: 'http://localhost:4000',
+        method: 'post',
+        data: {
+          query: `
+            mutation ($item: ItemInput) {
+              newPrefix: saveItem(item: $item) {
+                id
+                type
+                description
+              }
+            }
+          `,
+          variables: {
+            item: {
+              type: 'prefix',
+              description: prefix,
+            },
+          },
+        },
+      })
+      .then(response =>{
+        const query = response.data;
+        const newPrefix = query.data.newPrefix;
+        this.prefixes.push(newPrefix.description);
+      });
+    },
+    addSufix(sufix) {
+      this.sufixes.push(sufix);
+    },
+    deletePrefix(prefix) {
+      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+    },
+    deleteSufix(sufix) {
+      this.sufixes.splice(this.prefixes.indexOf(sufix), 1);
+    },
+    getPrefixes() {
+      axios({
+        url: 'http://localhost:4000',
+        method: 'post',
+        data: {
+          query: `
+          {
+            prefixes: items (type: "prefix") {
+              id
+              type
+              description
+            }
+          }
+        `,
+        },
+      }).then((response) => {
+        const query = response.data;
+        this.prefixes = query.data.prefixes;
+      });
+    },
+    getSufixes() {
+      axios({
+        url: 'http://localhost:4000',
+        method: 'post',
+        data: {
+          query: `
+          {
+            sufixes: items (type: "sufix") {
+              id
+              type
+              description
+            }
+          }
+        `,
+        },
+      }).then((response) => {
+        const query = response.data;
+        this.sufixes = query.data.sufixes;
+      });
+    },
+  },
+  computed: {
+    domains() {
+      console.log('generating domains...');
+      const domains = [];
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          const name = prefix.description + sufix.description;
+          const url = name.toLowerCase();
+          const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com`;
+          domains.push({
+            name,
+            checkout,
+          });
+        }
+      }
+      return domains;
+    },
+  },
+  created() {
+    this.getPrefixes();
+    this.getSufixes();
+  },
+};
+</script>
+
+<style></style>
