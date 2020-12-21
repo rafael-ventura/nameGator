89328badeaadf493babb1047fb9ea307b3cd3e61
Rafael Ventura <raf4ael@gmail.com>
2020-12-20 23:22:56 -0300

    adicionando arquivos servidor

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const { ApolloServer } = require('apollo-server');
+console.log(2);
+const typeDefs = `
+
+    type Item {
+        id: Int
+        type: String
+        description: String
+    }
+
+    type Query {
+        items(type: String): [Item]
+    }
+
+    input ItemInput {
+         type: String
+         description: String
+     }
+
+    type Mutation {
+        saveItem(item: ItemInput) : Item
+    }
+`;
+
+const items = [
+  { id: 1, type: 'prefix', description: 'Air' },
+  { id: 2, type: 'prefix', description: 'Water' },
+  { id: 3, type: 'prefix', description: 'Storm' },
+  { id: 4, type: 'sufix', description: 'Ice' },
+  { id: 5, type: 'sufix', description: 'Fire' },
+  { id: 6, type: 'sufix', description: 'Grass' },
+];
+
+const resolvers = {
+  Query: {
+    items(_, args) {
+      console.log(args);
+      return items.filter((item) => item.type === args.type);
+    },
+  },
+  Mutation: {
+    saveItem(_, args) {
+      const item = args.item;
+      item.id = Math.floor(Math.random() * 1000);
+      items.push(item);
+      return item;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen();
